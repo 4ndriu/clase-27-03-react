ee7ca5307658ac55c81ba538900a32bf0d89af40
4ndriu <andres.castillop@uniagustiniana.edu.co>
2026-03-27 19:17:31 +0000

    barra de inicio

---
+++ src/Favoritos/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+function Favorito(){
+    return(
+        <>
+            <p>Favorito</p>
+        </>
+    )
+}
+
+export default Favorito
---
+++ src/Informativa/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+function Infromativa(){
+    return(
+        <>
+            <p>Informativa</p>
+        </>
+    )
+}
+
+export default Infromativa
