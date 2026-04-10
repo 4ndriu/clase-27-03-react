efcb71ffece4ba1c43ac3625d7febccb8d03a20c
4ndriu <andres.castillop@uniagustiniana.edu.co>
2026-04-10 19:21:54 +0000

    solo millos

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router'
 
 interface Ranking {
   rank: number
-  contestantName: string
+  contestantName: string 
   points: number
   matchesPlayed: number
 }
 
+
 function Home() {
   const [ranking, setRanking] = useState<Ranking[]>([])
   const [title, setTitle] = useState('')
+
+  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +31,46 @@ function Home() {
     fetchData()
   }, [])
 
+  type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
+
+  const equiposMap: Record<string, string> = {
+  "América de Cali SA": "america-de-cali",
+  "AD Pasto": "ad-pasto",
+  "CA Bucaramanga": "atletico-bucaramanga",
+  "Club Atlético Nacional SA": "atletico-nacional",
+  "Club Deportes Tolima SA": "deportes-tolima",
+  "Asociación Deportivo Cali": "deportivo-cali",
+  "Deportivo Independiente Medellín": "independiente-medellin",
+  "Club Independiente Santa Fe": "independiente-santa-fe",
+  "CD Popular Junior FC SA": "junior",
+  "Millonarios FC": "millonarios",
+  "Once Caldas SA": "once-caldas",
+
+  "Internacional de Bogotá": "internacional-bogota",
+  "Club Llaneros SA": "llaneros",
+  "Águilas Doradas": "aguilas-doradas",
+  "Fortaleza FC": "fortaleza",
+  "Alianza FC": "alianza",
+  "Jaguares de Córdoba FC": "jaguares",
+  "Cúcuta Deportivo FC": "cucuta",
+  "Boyacá Chicó FC": "boyaca-chico",
+  "Deportivo Pereira FC": "pereira"
+};
+
   return (
+<>
+    <div className="filtros">
+        {filtros.map((onestat) => (
+          <button
+            key={onestat}
+            onClick={() => setFiltro(onestat)}
+            className={filtro === onestat ? 'activo' : ''}
+          >
+            {onestat}
+          </button>
+        ))}
+      </div>
+
     <div className="tabla-container">
       <h2>{title}</h2>
       <table className="tabla-posiciones">
@@ -43,7 +86,9 @@ function Home() {
           {ranking.map((equipo) => (
             <tr key={equipo.rank}>
               <td>{equipo.rank}</td>
-              <td>{equipo.contestantName}</td>
+              <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
+                        {equipo.contestantName}
+                      </Link>
               <td>{equipo.matchesPlayed}</td>
               <td>{equipo.points}</td>
             </tr>
@@ -51,7 +96,9 @@ function Home() {
         </tbody>
       </table>
     </div>
+    </>
   )
+  
 }
 
 export default Home
